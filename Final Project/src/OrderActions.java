a7f8f8827bbd74de99b6e63ed83a6a8873b1fb43
cwilson2020 <70825951+cwilson2020@users.noreply.github.com>
2020-10-04 04:16:17 -0400

    added helper order class using state design pattern

---
+++ Final Project/src/OrderActions.java
@@ -0,0 +1,21 @@
+
+
+public interface OrderActions {
+
+	
+	/*void CancelledOrder();
+	void SubmitOrder();
+	void FulfillOrder();
+	void BuildOutOrder();*/
+	
+	
+	 void editOrder(int OrderNumber);
+	 void cancelOrder();
+	void  copyOrder(int OrderNumber);
+	 void fulfillOrder();
+	 void submitOrder();
+	
+	
+	
+
+}
